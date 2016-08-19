e02695ebb3c15df0cf8e76cb64e439dba703ea8a
Andrii Vasyliev <sol@hiqdev.com>
2016-08-19 15:30:53 +0000

    csfixed

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * AHnames asset files: images, configs and more
+ * HiQDev asset files: images, configs and more
  *
- * @link      https://github.com/ahnames/yii2-asset-ahnames
- * @package   yii2-asset-ahnames
- * @license   proprietary
- * @copyright Copyright (c) 2015-2016, AHnames (https://ahnames.com/)
+ * @link      https://github.com/hiqdev/yii2-asset-hiqdev
+ * @package   yii2-asset-hiqdev
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
  */
 
 return [];
--- src/config/params.php
+++ src/config/params.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * AHnames asset files: images, configs and more
+ * HiQDev asset files: images, configs and more
  *
  * @link      https://github.com/hiqdev/yii2-asset-hiqdev
  * @package   yii2-asset-hiqdev
- * @license   proprietary
- * @copyright Copyright (c) 2015-2016, AHnames (https://hiqdev.com/)
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
  */
 
 return [
